df013b93d12d6bbdc534d585eedf5416b7f00113
Peter Shin <petershin731@gmail.com>
2022-01-06 12:04:58 -0800

    first commit

---
+++ backend/db/seeders/20220106185538-demo-user.js
@@ -0,0 +1,31 @@
+'use strict';
+const bcrypt = require('bcryptjs');
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Users', [
+      {
+        email: 'demo@user.io',
+        username: 'demo',
+        hashedPassword: bcrypt.hashSync('password'),
+      },
+      {
+        email: 'demo1@user.io',
+        username: 'FakeUser1',
+        hashedPassword: bcrypt.hashSync('password1'),
+      },
+      {
+        email: 'demo2@user.io',
+        username: 'FakeUser2',
+        hashedPassword: bcrypt.hashSync('password2'),
+      },
+    ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete('Users', {
+      username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
+    }, {});
+  }
+};
