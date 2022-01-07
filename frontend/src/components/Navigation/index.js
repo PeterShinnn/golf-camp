c840560cd8c0d1296b4e298a235a55a0400960df
Peter Shin <petershin731@gmail.com>
2022-01-06 16:56:20 -0800

    finished navbar and slogan

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,34 +1,52 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import LoginFormModal from '../LoginFormModal';
+import Slogan from '../Slogan';
 import './Navigation.css';
 
-function Navigation({ isLoaded }){
+function Navigation({ isLoaded }) {
   const sessionUser = useSelector(state => state.session.user);
 
   let sessionLinks;
   if (sessionUser) {
     sessionLinks = (
-      <ProfileButton user={sessionUser} />
+      // displays the links when the user is logged in
+      <>
+        <span>
+          <NavLink to="/trips" className="navLinks">Trips</NavLink>
+          <NavLink to="/saves" className="navLinks">Saves</NavLink>
+          <NavLink to="/inbox" className="navLinks">Inbox</NavLink>
+          <ProfileButton user={sessionUser} />
+        </span>
+      </>
     );
   } else {
     sessionLinks = (
+      // displays the links when the user is logged out 
       <>
-        <LoginFormModal />
-        <NavLink to="/signup">Sign Up</NavLink>
+        <span>
+          <NavLink to="/near" className="navLinks">Near Me</NavLink>
+          <NavLink to="/about" className="navLinks">About</NavLink>
+          <NavLink to="/earn" className="navLinks">Earn Birdy</NavLink>
+          <LoginFormModal />
+          <NavLink to="/signup" className="navLinks">Sign Up</NavLink>
+        </span>
       </>
     );
   }
 
   return (
-    <ul>
-      <li>
-        <NavLink exact to="/">Home</NavLink>
+    <>
+      <nav>
+        <Link exact to="/" className="logo-link">GOLFCAMP</Link>
         {isLoaded && sessionLinks}
-      </li>
-    </ul>
+      </nav>
+      <navHeader>
+        <Slogan />
+      </navHeader>
+    </>
   );
 }
 
